db42a8bac3aa6fb655e25a9f0cc69879c3dec3b5
Aditya Singh <ab.adityasingh2001@gmail.com>
2021-08-05 12:10:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/aditya/draweractivity1/BottomNavigationActivity.java
@@ -0,0 +1,31 @@
+package com.aditya.draweractivity1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+        BottomNavigationView bottomNav=findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                if(item.getItemId()==R.id.action_delete){
+                    Toast.makeText(BottomNavigationActivity.this, "Delete is clicked", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+    }
+}
--- app/src/main/java/com/aditya/draweractivity1/MainActivity.java
+++ app/src/main/java/com/aditya/draweractivity1/MainActivity.java
@@ -7,13 +7,18 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.time.temporal.TemporalAccessor;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
@@ -32,14 +37,34 @@ private DrawerLayout drawerLayout;
 
         NavigationView navView = findViewById(R.id.sliding_navigation);
         navView.setNavigationItemSelectedListener(this);
+        View headerView=navView.getHeaderView(0);
+        TextView tvUser=headerView.findViewById(R.id.tv_header);
+        tvUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Header is Clciked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
     @Override
     public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
         drawerLayout.closeDrawer(GravityCompat.START);
-        if (item.getItemId() == R.id.action_move) {
-            Toast.makeText(MainActivity.this, "Move Clicked", Toast.LENGTH_LONG).show();
-        }
-        return false;
+       switch (item.getItemId()){
+           case R.id.action_edit:
+               startActivity(new Intent(MainActivity.this,BottomNavigationActivity.class));
+               break;
+           case R.id.action_delete:
+               Toast.makeText(this, "Delete is Clicked", Toast.LENGTH_SHORT).show();
+               break;
+           case R.id.action_move:
+               Toast.makeText(this, "Move is Clicked", Toast.LENGTH_SHORT).show();
+               break;
+           default:
+               Toast.makeText(this, "Setting is clicked", Toast.LENGTH_SHORT).show();
+               break;
+
+       }
+        return true;
     }
 }
